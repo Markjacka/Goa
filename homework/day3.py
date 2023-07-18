d14c7577d3dcfe8c6064397226ef5892048a02f8
Markjacka <lmzavanadze@bk.ru>
2023-07-18 17:16:30 +0300

    first commit

---
+++ homework/day3.py
@@ -0,0 +1,11 @@
+current_age = 15
+travel_years = 20
+after_travel = current_age + travel_years
+print(after_travel)
+
+current_age = input("რამდენი წლის ხართ? ")
+travel_years = input("რამდენი წელი გსურთ მოგზაურობაში? ")
+future_age = current_age + travel_years
+print("დროში მოგზაურობის შემდეგ თქვენი ასაკი იქნება:", future_age)
+
+
