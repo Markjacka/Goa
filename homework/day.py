3463f0cf1056c5cdef7c32c4b7b156dfd9fb8953
Markjacka <lmzavanadze@bk.ru>
2023-07-21 15:44:42 +0300

    commit

---
+++ homework/day.py
@@ -0,0 +1,9 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favorite hobby? ")
+
+# Construct the sentence using string concatenation
+sentence = "Hello, my name is " + name + ". I am " + age + " years old, and my favorite hobby is " + hobby + "."
+
+# Print the sentence
+print(sentence)
