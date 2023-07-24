99fe55798ea4c005fd02b2bbb4dc40838bade621
Markjacka <lmzavanadze@bk.ru>
2023-07-24 22:35:20 +0300

    fifth commit

---
+++ homework/day2.py
@@ -0,0 +1,88 @@
+prnt("Hello, world!"  # Missing closing parenthesis
+print "This is a test."  # Missing parentheses for the print statement
+Print("Another test.")  # Capitalization error
+print("I am missing a closing quotation mark.)  # Missing closing quotation mark
+print('I am missing an opening quotation mark.)  # Missing opening quotation mark
+print("I have a mismatched quotation mark.')  # Mismatched opening and closing quotation marks
+print('I have a mismatched quotation mark.")  # Mismatched opening and closing quotation marks
+print("I forgot to close this string.)  # Missing closing quotation mark
+print("This is a string with a newline character.\n")  # Unclosed parentheses
+print("This is a string with a tab character.\t")  # Unclosed parentheses
+print("This is a string with a backslash character.\")  # Unclosed parentheses
+print("This is a string with a single quote (').")  # Unclosed parentheses
+print("This is a string with a double quote (\").")  # Unclosed parentheses
+print("This is a string with a backslash (\\).")  # Unclosed parentheses
+print("This is a string with a bell character (\a).")  # Unclosed parentheses
+print("This is a string with a backspace character (\b).")  # Unclosed parentheses
+print("This is a string with a form feed character (\f).")  # Unclosed parentheses
+print("This is a string with a carriage return character (\r).")  # Unclosed parentheses
+print("This is a string with a vertical tab character (\v).")  # Unclosed parentheses
+print("This is a string with an octal value (\123).")  # Unclosed parentheses
+print("This is a string with a hexadecimal value (\x41).")  # Unclosed parentheses
+print("This is a string with a Unicode character (\u03A9).")  # Unclosed parentheses
+print("his is a string with an invalid escape sequence (\z).")  # Unclosed parentheses
+print("ThiTs is a string with a variable substitution: %s" % "test")  # Unclosed parentheses
+print("This is a string with a variable substitution: {}".format("test")  # Unclosed parentheses
+print("This is a string with a variable substitution: {test}")  # Unclosed parentheses
+print(": {test".format("test"))  # Unclosed parentheses
+print("This is a string with a variable substitution: {test}".format("test")  # Missing closing parentheses
+print("This is a string with a variable substitution: {test}".format("test"))  # Extra closing parenthesis
+print("This is a string with a variable substitution: {test}".format("test"))))  # Extra closing parentheses
+print("This is a string with a variable substitution: {test}".format(test="test")  # Missing closing parentheses
+print("This is a string with a variable substitution: {test}".format(test="test")))  # Extra closing parentheses
+print("This is a string with a variable substitution: {test}".format(test="test"))))  # Extra closing parentheses
+print("This is a string with a variable substitution: {test}".format(test="test"))))  # Extra closing parentheses
+print("This is a string with a variable substitution: {test}".format(test="test")  # Missing closing parentheses
+print("This is a string with a variable substitution: {test}".format(test="test")))  # Extra closing parentheses
+print("This is a string with a variable substitution: {test}".format(test="test"))))  # Extra closing parentheses
+print("This is a string with a variable substitution: {test}".format(test="test"))))  # Extra closing parentheses
+print("This is a string with a variable substitution: {test}".format(test="test")  # Missing closing parentheses
+print("This is a string with a variable substitution: {test}".format(test="test")))  # Extra closing parentheses
+print("This is a string with a variable substitution: {test}".format(test="test"))))  # Extra closing parentheses
+print("This is a string with a variable substitution: {test}".format(test="test"))))  # Extra closing parentheses
+print("This is a string with a variable substitution: {test}".format(test="test")  # Missing closing parentheses
+print("This is a string with a variable substitution: {test}".format(test="test")))  # Extra closing parentheses
+print("This is a string with a variable substitution: {test}".format(test="test"))))  # Extra closing parentheses
+print("This is a string with a variable substitution: {test}".format(test="test"))))  # Extra closing parentheses
+print("This is a string with a variable substitution: {test}".format(test="test")  # Missing closing parentheses
+print("This is a string with a variable substitution: {test}".format(test="test")))  # Extra closing parentheses
+print("This is a string with a variable substitution: {test}".format(test="test"))))  # Extra closing parentheses
+print("This is a string with a variable substitution: {test}".format(test="test"))))  # Extra closing parentheses
+print("This is a string with a variable substitution: {test}".format(test="test")  # Missing closing parentheses
+print("This is a string with a variable substitution: {test}".format(test="test")))  # Extra closing parentheses
+print("This is a string with a variable substitution: {test}".format(test="test"))))  # Extra closing parentheses
+print("This is a string with a variable substitution: {test}".format(test="test"))))  # Extra closing parentheses
+
+
+
+
+
+
+
+print("Hello, world!")  # Corrected: Added closing parenthesis
+print("This is a test.")  # Corrected: Added parentheses for the print statement
+print("Another test.")  # Corrected: Changed 'Print' to 'print' (capitalization)
+print("I am missing a closing quotation mark.")  # Corrected: Added closing quotation mark
+print('I am missing an opening quotation mark.')  # Corrected: Added opening quotation mark
+print("I have a mismatched quotation mark.")  # Corrected: Removed mismatched quotation marks
+print('I have a mismatched quotation mark.')  # Corrected: Removed mismatched quotation marks
+print("I forgot to close this string.")  # Corrected: Added closing quotation mark
+print("This is a string with a newline character.\n")  # Corrected: Added closing parenthesis
+print("This is a string with a tab character.\t")  # Corrected: Added closing parenthesis
+print("This is a string with a backslash character.\"")  # Corrected: Added closing parenthesis
+print("This is a string with a single quote (').")  # Corrected: Added closing parenthesis
+print("This is a string with a double quote (\").")  # Corrected: Added closing parenthesis
+print("This is a string with a backslash (\\).")  # Corrected: Added closing parenthesis
+print("This is a string with a bell character (\a).")  # Corrected: Added closing parenthesis
+print("This is a string with a backspace character (\b).")  # Corrected: Added closing parenthesis
+print("This is a string with a form feed character (\f).")  # Corrected: Added closing parenthesis
+print("This is a string with a carriage return character (\r).")  # Corrected: Added closing parenthesis
+print("This is a string with a vertical tab character (\v).")  # Corrected: Added closing parenthesis
+print("This is a string with an octal value (\123).")  # Corrected: Added closing parenthesis
+print("This is a string with a hexadecimal value (\x41).")  # Corrected: Added closing parenthesis
+print("This is a string with a Unicode character (\u03A9).")  # Corrected: Added closing parenthesis
+print("This is a string with an invalid escape sequence (\\z).")  # Corrected: Added closing parenthesis
+print("This is a string with a variable substitution: %s" % "test")  # Corrected: Added closing parenthesis
+print("This is a string with a variable substitution: {}".format("test"))  # Corrected: Added closing parenthesis
+print("This is a string with a variable substitution: {test}".format(test="test"))  # Corrected: Added closing parenthesis
+print("This is a string with a variable substitution: {test}".format("test"))  # Corrected: Removed unnecessary closing parenthesis
---
+++ homework/day4.py
@@ -0,0 +1,3 @@
+age = int(input("შეიყვანეთ თქვენი ასაკი: "))
+is_under_18 = age < 18
+print(is_under_18)
