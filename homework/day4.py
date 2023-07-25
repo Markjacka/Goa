0dd7b6edd12ebbf687d4e15fc87beaf839d4d527
sandro oqropiridze <107420831+Markjacka@users.noreply.github.com>
2023-07-25 12:48:49 +0300

    Update day4.py

--- homework/day4.py
+++ homework/day4.py
@@ -1,3 +1,13 @@
 age = int(input("შეიყვანეთ თქვენი ასაკი: "))
 is_under_18 = age < 18
 print(is_under_18)
+
+
+father_age = int(input("შეიყვანეთ მამისის ასაკი: "))
+user_age = int(input("შეიყვანეთ თქვენი ასაკი: "))
+
+father_over_60 = father_age > 60
+user_over_60 = user_age > 60
+
+print("მამისი არის 60 წელზე მეტი:", father_over_60)
+print("თქვენი არის 60 წელზე მეტი:", user_over_60)
